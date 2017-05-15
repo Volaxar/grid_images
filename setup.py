5d52e3b4cb9fadd2a1e9de1bd53cd87b4b994e95
Volaxar <volaxar@gmail.com>
2017-05-15 14:37:13 +0500

    Package ready v0.0.1

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-grid-images',
+    version='0.0.1',
+    packages=['grid_images'],
+    include_package_data=True,
+    license='BSD License',
+    description='',
+    long_description=README,
+    author='Volaxar',
+    author_email='volaxar@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
